42a42751cac75fab4fb01505294be6a3ef25268e
Omalicha <ada.oyom@gmail.com>
2017-04-04 09:41:46 +0100

    Reconstructed code into functions

--- Number Guess Game.py
+++ Number Guess Game.py
@@ -7,34 +7,43 @@ import random
 
 print "==========Welcome==========="
 print "\nI Think of a number between 1-50 and All you have to do is make a simple guess"
-print "I\'ll tell you if you are right or wrong, you have 5 chances"
+print "\nI\'ll tell you if you are right or wrong, you have 5 chances"
 
-correct_number = random.randrange(50) + 1
-number_guess = int(raw_input("Take a guess from 1-50: "))
-tries = 1
-count = 0
+# correct_number = random.randrange(50) + 1
+# number_guess = int(raw_input("Take a guess from 1-50: "))
+# tries = 0
+# count = 0
 
-while number_guess != correct_number:
-	if tries ==(3)+1:
-		print "\n\n \Sorry You have exceeded the number of chances given to you"
-		break
-	# print "========================="
-	# print "\n\nSorry, make another guess: \n"
+def correct_guess(correct_number,tries): 
+	#if number_guess == correct_number:
+		print "Yes!, you guessed it right! The number was ", correct_number
+		print "Number of tries: ", tries
+		print "\n\n================Goodbye=================="
+def number():
+	#while number_guess != correct_number:
+	correct_number = random.randrange(50) + 1
+	number_guess = -1
+	tries = 0
+	count = 0
+	#print correct_number
+	while number_guess != correct_number:
+		tries += 1
+		count += 1
+		if tries == 5:
+			print "\n\n \Sorry You have exceeded the number of chances given to you"
+			break
+		number_guess = int(raw_input("Take a guess from 1-50: "))
+		if number_guess > correct_number:
+			print "========================="
+			print "\n\nSorry that is a bit high, make another guess: \n"
 
-	if number_guess > correct_number:
-		print "========================="
-		print "\n\nSorry that is a bit high, make another guess: \n"
+		elif number_guess < correct_number:
+			print "========================="
+			print "\n\n Oops That\'s Lesser Than what i\'m thinking of, make another try: \n"
 
-	elif number_guess < correct_number:
-		print "========================="
-		print "\n\n Oops That\'s Lesser Than what i\'m thinking of, make another try: \n"
+		else:
+			correct_guess(correct_number,tries);
+	# tries += 1
+	# count += 1
 
-	number_guess = int(raw_input("Take a guess from 1-50: "))
-	tries =+1
-	count =+1
-
-
-if number_guess == correct_number:
-	print "Yes!, finally you guessed it right! The number was ", correct_number
-	print "Number of tries: ", tries
-
+number()
